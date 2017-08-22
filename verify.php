80bd9bece4e0ae8925101cbe2afffba8ef9ff04f
dechaphan <dechaphan@gmail.com>
2017-08-22 20:54:54 +0700

    edit #5

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php
-$access_token = 'bf8thJdpUeyrq+7itXGC1jil3JZ3KQ2bmPUOw0rWSEpzJITvOyyVMjI/M/lBYp///vzRUpI6L2e9ubC01+qPs0Epor2PI0SZBwsf1W72rdomM+ETSa8lJMq/ekDUcsxwf0cvnUvXue4ln1ymRz6NyAdB04t89/1O/w1cDnyilFU=';
-$proxy = 'velodrome.usefixie.com:80';
-$proxyauth = 'fixie:8GxkUEXnPKM7dGj';
+$access_token = '';
+$proxy = '';
+$proxyauth = '';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
