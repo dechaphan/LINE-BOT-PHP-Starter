ca4b72d269273303e18a6d512bbb576717b708ec
dechaphan <dechaphan@gmail.com>
2017-08-22 20:26:53 +0700

    edit #2

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'bf8thJdpUeyrq+7itXGC1jil3JZ3KQ2bmPUOw0rWSEpzJITvOyyVMjI/M/lBYp///vzRUpI6L2e9ubC01+qPs0Epor2PI0SZBwsf1W72rdomM+ETSa8lJMq/ekDUcsxwf0cvnUvXue4ln1ymRz6NyAdB04t89/1O/w1cDnyilFU=';
+$access_token = '';
 
 // Get POST body content
 $content = file_get_contents('php://input');
