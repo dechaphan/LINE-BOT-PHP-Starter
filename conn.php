751ea9a7ae6f314ead912069093903b6f9c6b93f
dechaphan <dechaphan@gmail.com>
2017-08-22 21:45:38 +0700

    edit #7

---
+++ conn.php
@@ -0,0 +1,4 @@
+<?php
+$access_token = 'bf8thJdpUeyrq+7itXGC1jil3JZ3KQ2bmPUOw0rWSEpzJITvOyyVMjI/M/lBYp///vzRUpI6L2e9ubC01+qPs0Epor2PI0SZBwsf1W72rdomM+ETSa8lJMq/ekDUcsxwf0cvnUvXue4ln1ymRz6NyAdB04t89/1O/w1cDnyilFU=';
+$proxy = 'velodrome.usefixie.com:80';
+$proxyauth = 'fixie:8GxkUEXnPKM7dGj';
